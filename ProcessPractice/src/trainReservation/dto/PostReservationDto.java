2de0fa29e8874d5b8d66699d89282979b1c421ad
Newlimited <sbh2043@naver.com>
2023-03-15 10:39:55 +0900

    23.03.15 10:40

--- ProcessPractice/src/trainReservation/dto/PostReservationDto.java
+++ ProcessPractice/src/trainReservation/dto/PostReservationDto.java
@@ -1,19 +1,37 @@
 package trainReservation.dto;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class PostReservationDto {
 	private String trainNumber;
 	private List<String> seats;
 
-	
-	
-	
-	
-	
-	
-	public PostReservationDto() {
+	public PostReservationDto(int numberOfPeople) {
 
+		Scanner scanner = new Scanner(System.in);
+		this.seats = new ArrayList<>();
+
+		while (true) {
+
+			System.out.println("탑승 할 열차 번호 :");
+			this.trainNumber = scanner.nextLine();
+			if (this.trainNumber.isBlank())	{
+				System.out.println("열차 번호를 입력하세요");
+				continue;
+			}
+			break;
+		}
+		while (this.seats.size() < numberOfPeople) {
+			System.out.println("좌석 번호 : ");
+			String seat = scanner.nextLine();
+			if (seat.isBlank())	{
+				System.out.println("좌석 번호를 입력하세요.");
+				continue;
+			}
+			this.seats.add(seat);
+		}
 	}
 
 	public PostReservationDto(String trainNumber, List<String> seats) {
@@ -24,7 +42,6 @@ public class PostReservationDto {
 	public String getTrainNumber() {
 		return trainNumber;
 	}
-
 
 	public void setTrainNumber(String trainNumber) {
 		this.trainNumber = trainNumber;
@@ -37,6 +54,7 @@ public class PostReservationDto {
 	public void setSeats(List<String> seats) {
 		this.seats = seats;
 	}
+
 	@Override
 	public String toString() {
 		return "PostReservationDto [trainNumber=" + trainNumber + ", seats=" + seats + "]";
